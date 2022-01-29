7ce2c512e5a22b0e3ba5bf0c14d07255c8826df7
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2022-01-29 10:08:28 -0300

    stats

---
+++ stats.js
@@ -0,0 +1,17 @@
+const os = require("os");
+
+const { freemem, totalmem } = os;
+
+setInterval(() => {
+    const total = parseInt(totalmem() / 1024 / 1024);
+    const mem = parseInt(freemem() / 1024 / 1024);
+    const percents = parseInt((mem / total) * 100);
+
+    const stats = {
+        total: `${total} MB`,
+        free: `${mem} MB`,
+        percent: `${percents}%`,
+    };
+    console.clear();
+    console.table(stats);
+}, 1000);
